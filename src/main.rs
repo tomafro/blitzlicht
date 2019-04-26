be88886a86904cd82daf2cce98d61f98125edaef
Tom Ward <tom@popdog.net>
2019-04-26 19:53:57 +0100

    Rejig runner

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,14 @@ use blitzlicht::*;
 use blitzlicht::io::Reader;
 use blitzlicht::matcher::Matcher;
 use blitzlicht::runner::Runner;
-use blitzlicht::printer::{ Printer, BasicPrinter };
-
-use std::collections::HashSet;
 
 fn main() -> Result<()> {
     let path = "/Users/tom/Work/basecamp/haystack/log/development.log";
 
-    let runner = Runner {
-        reader:  Reader::file(path, false)?,
-        matcher: Matcher::new(),
-        printer: BasicPrinter::new(),
-        matches: HashSet::new()
-    };
+    let runner = Runner::new(
+        Reader::file(path, false)?,
+        Matcher::new()
+    );
 
     runner.run()
 }
--- src/runner.rs
+++ src/runner.rs
@@ -7,43 +7,47 @@ use crate::printer::{ Printer, BasicPrinter };
 use std::collections::HashSet;
 
 pub struct Runner {
-    pub reader: Reader,
-    pub matcher: Matcher,
-    pub printer: BasicPrinter,
-    pub matches: HashSet<String>
-}
-
-fn matched_line(matches: &mut HashSet<String>, printer: &mut Printer, unparsed: &String) {
-    match Line::parse(unparsed) {
-        Some(line) => {
-            matches.insert(line.id.to_owned());
-            printer.matched(&line);
-        },
-        None    => printer.unrecognised(unparsed)
-    }
-}
-
-fn unmatched_line(matches: &HashSet<String>, printer: &mut Printer, unparsed: &String) {
-    match Line::parse(&unparsed) {
-        Some(line) => {
-            match matches.contains(line.id) {
-                true  => printer.matched_id(&line),
-                false => printer.unmatched(&line)
-            }
-        }
-        None    => printer.unrecognised(&unparsed)
-    }
+    reader: Reader,
+    matcher: Matcher,
+    printer: BasicPrinter,
+    matches: HashSet<String>
 }
 
 impl Runner {
+    pub fn new(reader: Reader, matcher: Matcher) -> Runner {
+        Runner { reader, matcher, printer: BasicPrinter::new(), matches: HashSet::new() }
+    }
+
     pub fn run(mut self) -> Result<()> {
         for line in self.reader {
             match self.matcher.matches(&line) {
-                true  => matched_line(&mut self.matches, &mut self.printer, &line),
-                false => unmatched_line(&self.matches, &mut self.printer, &line)
+                true  => Self::matched_line(&mut self.matches, &mut self.printer, &line),
+                false => Self::unmatched_line(&self.matches, &mut self.printer, &line)
             }
         }
 
         Ok(())
+    }
+
+    fn matched_line(matches: &mut HashSet<String>, printer: &mut Printer, unparsed: &String) {
+        match Line::parse(unparsed) {
+            Some(line) => {
+                matches.insert(line.id.to_owned());
+                printer.matched(&line);
+            },
+            None    => printer.unrecognised(unparsed)
+        }
+    }
+
+    fn unmatched_line(matches: &HashSet<String>, printer: &mut Printer, unparsed: &String) {
+        match Line::parse(&unparsed) {
+            Some(line) => {
+                match matches.contains(line.id) {
+                    true  => printer.matched_id(&line),
+                    false => printer.unmatched(&line)
+                }
+            }
+            None    => printer.unrecognised(&unparsed)
+        }
     }
 }
